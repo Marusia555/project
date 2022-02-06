855771cbc79ecaa27a53d6b3ceacae4d835b54a6
Marusia555 <s.kut.er1208@yandex.ru>
2022-02-07 00:34:43 +0300

    lesson12

--- js/script.js
+++ js/script.js
@@ -1,30 +1,44 @@
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
 "use strict";
 
-//alert('Hello');
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
 
-//const result = confirm("Are you here?");
-//console.log(result);
+const personalMovieDB = {
+    count: numberOfFilms,
+    muvies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-//const answer = +prompt("You is 18?","18");
-//console.log(answer+5);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-const answers = [];
+      personalMovieDB.muvies[a] = b;
+      personalMovieDB.muvies[c] = d;
 
-answers[0] = prompt('What is your name?','');
-answers[1] = prompt('What is your last name?','');
-answers[2] = prompt('How old are you?','');
-
-document.write(answers);
-
-let incr = 10,
-    decr = 10;
-
-    //incr++;
-    //decr--;
-
-    console.log(++incr);
-    console.log(--decr);
-
-    console.log(5%2);
-
-    console.log(2*4 === 8);
+      console.log (personalMovieDB);
